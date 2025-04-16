bdaaaa82a0c59fba2863cb3de969496127f6e21d
arjun <arjun.m010011@gmail.com>
2025-04-16 20:49:58 +0530

    created selection page with responsive design

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -4,31 +4,57 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import { Menu, X } from "lucide-react";
-
+import { motion } from "framer-motion";
+import Link from "next/link";
 const Header = () => {
   const [open, setOpen] = useState(false);
+  const variant = {
+    hidden: { x: "150%" },
+    visible: { x: 0 },
+    exit: { x: "150%" },
+  };
   return (
-    <header className="flex w-[100vw] justify-between items-center px-20 max-md:px-10">
+    <header className="flex w-[100vw] justify-between items-center px-20 max-md:px-10 overflow-x-hidden">
       <Image src="/logo.png" height={70} width={70} alt="logo" />
       <div
         className={`${open ? "bg-white" : "bg-transparent"} flex flex-col absolute right-0 top-0  p-5 h-full md:static md:flex-row  `}
       >
         <button
-          className={`md:hidden text-black `}
+          className={`md:hidden text-black absolute right-5 `}
           onClick={() => setOpen(!open)}
         >
           {open ? <X size={30} /> : <Menu size={30} />}
         </button>
-        <ul
-          className={`${open ? "block" : "hidden"} flex flex-col gap-2 py-5 pr-10 pl-2 md:flex md:items-center md:flex-row md:gap-5 md:p-5`}
+        <motion.ul
+          initial="hidden"
+          animate={open ? "visible" : "hidden"}
+          exit="hidden"
+          variants={variant}
+          transition={{ type: "tween", duration: "0.5" }}
+          className={`${open ? "block" : "hidden"} flex flex-col gap-2 py-5 pr-10 pl-2 mt-7 md:hidden`}
         >
-          <li>Home</li>
+          <Link href="/">
+            <li>Home</li>
+          </Link>
           <li>pricing</li>
           <li>About</li>
           <li>Features</li>
           <li>FAQ</li>
           <li>Contact</li>
           <li className="mt-5 md:mt-0">
+            <Button variant="default">Get started</Button>
+          </li>
+        </motion.ul>
+        <ul className="hidden md:flex md:items-center md:gap-5">
+          <Link href="/">
+            <li>Home</li>
+          </Link>
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Features</li>
+          <li>FAQ</li>
+          <li>Contact</li>
+          <li>
             <Button variant="default">Get started</Button>
           </li>
         </ul>
---
+++ app/selection/page.jsx
@@ -0,0 +1,117 @@
+"use client";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { motion } from "framer-motion";
+import {
+  GraduationCapIcon,
+  Check,
+  TrendingUpIcon,
+  UserIcon,
+  TrophyIcon,
+  GroupIcon,
+} from "lucide-react";
+import { Button } from "@/components/ui/button";
+const page = () => {
+  return (
+    <div className="max-w-full overflow-x-hidden">
+      <Header />
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
+        <div className="w-full h-auto px-5 py-10 flex flex-col gap-4 text-center">
+          <p className="font-extrabold text-4xl md:text-7xl">
+            Choose your path
+          </p>
+          <p>
+            Join our educational platform and unlock your potential. Select your
+            role to get started with personalized analytics and learning
+            experience.
+          </p>
+        </div>
+        <div className="p-5 flex flex-col gap-5 md:flex-row md:px-20 md:flex md:items-center md:justify-center ">
+          <div className="border border-black rounded-lg p-5 flex flex-col gap-3  md:pr-50">
+            <div className="p-5 bg-slate-100 w-fit rounded-full">
+              <GraduationCapIcon size={30} />
+            </div>
+            <p className="text-4xl font-extrabold">Student</p>
+            <ul>
+              <li className="flex  gap-2 font-extralight">
+                <Check size={20} />
+                Track your learning progress
+              </li>
+              <li className="flex  gap-2  font-extralight">
+                <Check size={20} />
+                Access study materials
+              </li>
+              <li className="flex  gap-2 font-extralight">
+                <Check size={20} />
+                Connect with teachers
+              </li>
+            </ul>
+            <Button className="mt-5 md:w-[200px]">Signup as a Student</Button>
+          </div>
+          <div className="border border-black rounded-lg p-5 flex flex-col gap-3 md:pr-50">
+            <div className="p-5 bg-slate-100 w-fit rounded-full">
+              <GroupIcon size={30} />
+            </div>
+            <p className="text-4xl font-extrabold">Teacher</p>
+            <ul>
+              <li className="flex  gap-2 font-extralight">
+                <Check size={20} />
+                Monitor student progress
+              </li>
+              <li className="flex  gap-2  font-extralight">
+                <Check size={20} />
+                Create course content
+              </li>
+              <li className="flex  gap-2 font-extralight">
+                <Check size={20} />
+                Advanced analytics tools
+              </li>
+            </ul>
+
+            <Button className="mt-5 md:w-[200px]">Signup as a Teacher</Button>
+          </div>
+        </div>
+        <div className="">
+          <p className="text-5xl my-5 font-extrabold md:text-center md:mt-15">
+            Platform Features
+          </p>
+          <div className="p-5 flex flex-col gap-5 md:flex-row md:my-10">
+            <div className="p-5 border border-black rounded-lg flex flex-col gap-3">
+              <TrendingUpIcon size={30} />
+              <p className="text-2xl font-extrabold">
+                Performance and analytics
+              </p>
+              <p className="font-extralight">
+                Track progress and identify areas for improvement with detailed
+                analytics.
+              </p>
+            </div>
+            <div className="p-5 border border-black rounded-lg flex flex-col gap-3">
+              <UserIcon size={30} />
+              <p className="text-2xl font-extrabold">Interactive Learning</p>
+              <p className="font-extralight">
+                Engage in collaborative learning experiences with peers and
+                educators.
+              </p>
+            </div>
+            <div className="p-5 border border-black rounded-lg flex flex-col gap-3">
+              <TrophyIcon size={30} />
+              <p className="text-2xl font-extrabold">Achievement System</p>
+              <p className="font-extralight">
+                Earn badges and certificates as you progress through your
+                learning journey.
+              </p>
+            </div>
+          </div>
+        </div>
+      </motion.div>
+      <Footer />
+    </div>
+  );
+};
+
+export default page;
