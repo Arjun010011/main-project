6046c8c65dc669f6ccc94411b5b495df954ca3bc
arjun <arjun.m010011@gmail.com>
2025-05-08 22:41:34 +0530

    done with the ui of teacherDashboard header next to move on to the functionality

--- app/components/TeacherHeader.jsx
+++ app/components/TeacherHeader.jsx
@@ -1,19 +1,26 @@
 "use client";
 import Image from "next/image";
 import storeUser from "@/lib/store/userStore";
+import { Menu, Plus } from "lucide-react";
 const TeacherHeader = () => {
   const { userInfo } = storeUser();
   console.log(userInfo);
   return (
     <div>
-      <header>
-        <Image
-          src={userInfo.image || "/logo.png"}
-          height={70}
-          width={70}
-          alt="logo"
-          className="rounded-full"
-        />
+      <header className="flex m-5   items-center justify-between ">
+        <Menu size={30} />
+        <div className="flex items-center gap-8  ">
+          <div className="p-2 bg-gray-200 rounded-full">
+            <Plus size={20} />
+          </div>
+          <Image
+            src={userInfo.image || "/logo.png"}
+            height={40}
+            width={40}
+            alt="logo"
+            className="rounded-full"
+          />
+        </div>
       </header>
     </div>
   );
