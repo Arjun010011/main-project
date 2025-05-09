f8bf5c490303a15255afcba114e830af39662d7f
arjun <arjun.m010011@gmail.com>
2025-05-09 12:05:16 +0530

    working on plus button ui for now basic implementation is done moving forward for full ui implementiation

--- app/(auth)/(private)/teacherDashboard/page.jsx
+++ app/(auth)/(private)/teacherDashboard/page.jsx
@@ -1,12 +1,20 @@
 "use client";
 
 import storeUser from "@/lib/store/userStore";
+
+import { motion } from "framer-motion";
 import TeacherHeader from "@/app/components/TeacherHeader";
 const page = () => {
   const { userInfo } = storeUser();
   return (
     <div>
-      <TeacherHeader />
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transiton={{ duration: 3 }}
+      >
+        <TeacherHeader />
+      </motion.div>
       {userInfo?.fullName}
     </div>
   );
--- app/components/TeacherHeader.jsx
+++ app/components/TeacherHeader.jsx
@@ -2,15 +2,23 @@
 import Image from "next/image";
 import storeUser from "@/lib/store/userStore";
 import { Menu, Plus } from "lucide-react";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 const TeacherHeader = () => {
   const { userInfo } = storeUser();
   console.log(userInfo);
+  const [plusClick, setPlusClick] = useState(false);
   return (
-    <div>
-      <header className="flex m-5   items-center justify-between ">
+    <div className="w-full h-full flex items-center justify-center flex-col ">
+      <header className="flex p-5 w-full  items-center justify-between ">
         <Menu size={30} />
-        <div className="flex items-center gap-8  ">
-          <div className="p-2 bg-gray-200 rounded-full">
+        <div
+          className="flex items-center gap-8  "
+          onClick={() =>
+            plusClick === false ? setPlusClick(true) : setPlusClick(false)
+          }
+        >
+          <div className="p-2 rounded-full hover:bg-gray-100  hover:cursor-pointer transition duration-300 ease-in-out">
             <Plus size={20} />
           </div>
           <Image
@@ -22,6 +30,19 @@ const TeacherHeader = () => {
           />
         </div>
       </header>
+      <AnimatePresence>
+        {plusClick && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+            exit={{ opacity: 0 }}
+            className="p-5 bg-black text-white"
+          >
+            hello
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
