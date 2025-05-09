5b67b02810743640834da761f6c4a456341df6fe
arjun <arjun.m010011@gmail.com>
2025-05-09 20:17:44 +0530

    completed with  the ui of the plus icon now move on to functionality

--- app/components/TeacherHeader.jsx
+++ app/components/TeacherHeader.jsx
@@ -1,9 +1,10 @@
 "use client";
 import Image from "next/image";
 import storeUser from "@/lib/store/userStore";
-import { Menu, Plus } from "lucide-react";
+import { Menu, Plus, X } from "lucide-react";
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
+import { Button } from "@/components/ui/button";
 const TeacherHeader = () => {
   const { userInfo } = storeUser();
   console.log(userInfo);
@@ -37,9 +38,47 @@ const TeacherHeader = () => {
             animate={{ opacity: 1 }}
             transition={{ duration: 1 }}
             exit={{ opacity: 0 }}
-            className="p-5 bg-black text-white"
+            className=" absolute w-full h-full top-0 flex items-center justify-center bg-[rgba(0,0,0,0.5)]"
           >
-            hello
+            <div
+              className=" absolute w-full h-full top-0 flex items-center justify-center bg-[rgba(0,0,0,0.5)] z-0"
+              onClick={() => setPlusClick(false)}
+            ></div>
+            <form className="flex flex-col  px-10  py-10 bg-gray-100  z-50 w-auto h-auto rounded-lg md:w-[480px] ">
+              <div className="flex justify-between items-center ">
+                <p className="font-bold text-xl">Add a class </p>
+                <X
+                  size={25}
+                  onClick={() => setPlusClick(false)}
+                  className="hover:cursor-pointer"
+                />
+              </div>
+              <label htmlFor="className " className="mt-5">
+                Class name
+              </label>
+              <input
+                type="text"
+                placeholder="enter you className"
+                className="border-3 border-gray-200 rounded-lg px-2 py-2 mt-2 w-[70vw] md:w-full"
+              />
+              <label htmlFor="className " className="mt-5">
+                Subject
+              </label>
+              <input
+                type="text"
+                placeholder="enter you subject"
+                className="border-3 border-gray-200 rounded-lg px-2 py-2 mt-2 w-[70vw] md:w-full"
+              />
+              <label htmlFor="className " className="mt-5">
+                Section
+              </label>
+              <input
+                type="text"
+                placeholder="enter you section"
+                className="border-3 border-gray-200 rounded-lg px-2 py-2 mt-2 w-[70vw] md:w-full"
+              />
+              <Button className="mt-7 py-5">Create classroom</Button>
+            </form>
           </motion.div>
         )}
       </AnimatePresence>
