a907e34f506711cdb4f4d5ab07c6e4c24e5b949a
arjun <arjun.m010011@gmail.com>
2025-06-04 20:18:28 +0530

    fixed some ui bug in the header.jsx and intialized creatQuestionPaper.jsx page

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -5,18 +5,18 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import { Menu, X } from "lucide-react";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import Link from "next/link";
 const Header = () => {
   const [open, setOpen] = useState(false);
   const variant = {
     hidden: { x: "150%" },
-    visible: { x: 0 },
+    visible: { x: "-100%" },
     exit: { x: "150%" },
   };
   return (
     <header className="flex w-[100vw] justify-between items-center px-20 max-md:px-10 overflow-x-hidden dark:bg-gray-800">
-      <div className="flex gap-2 items-center justify-center flex-row-reverse">
+      <div className="flex gap-2 items-center justify-center flex-row-reverse max-md:mt-7 ">
         <div className="w-auto h-auto p-2">
           <ModeToggle />
         </div>
@@ -35,28 +35,30 @@ const Header = () => {
             <Menu size={30} className="dark:text-white" />
           )}
         </button>
-        <motion.ul
-          initial="hidden"
-          animate={open ? "visible" : "hidden"}
-          exit="hidden"
-          variants={variant}
-          transition={{ type: "tween", duration: "0.5" }}
-          className={`${open ? "block" : "hidden"} flex flex-col gap-2 py-5 pr-10 pl-2 mt-7 md:hidden dark:bg-gray-800`}
-        >
-          <Link href="/">
-            <li>Home</li>
-          </Link>
-          <li>pricing</li>
-          <li>About</li>
-          <li>Features</li>
-          <li>FAQ</li>
-          <li>Contact</li>
-          <Link href="/selection">
-            <li className="mt-5 md:mt-0">
-              <Button variant="default">Get started</Button>
-            </li>
-          </Link>
-        </motion.ul>
+        <AnimatePresence>
+          <motion.ul
+            initial="hidden"
+            animate={open ? "visible" : "hidden"}
+            exit="hidden"
+            variants={variant}
+            transition={{ type: "tween", duration: "0.5" }}
+            className={`fixed flex flex-col gap-2 py-5 pr-10  pl-7 h-[100vh]  mt-7 md:hidden dark:bg-gray-800`}
+          >
+            <Link href="/">
+              <li>Home</li>
+            </Link>
+            <li>pricing</li>
+            <li>About</li>
+            <li>Features</li>
+            <li>FAQ</li>
+            <li>Contact</li>
+            <Link href="/selection">
+              <li className="mt-5 md:mt-0">
+                <Button variant="default">Get started</Button>
+              </li>
+            </Link>
+          </motion.ul>
+        </AnimatePresence>
         <ul className="hidden md:flex md:items-center md:gap-5">
           <Link href="/">
             <li>Home</li>
