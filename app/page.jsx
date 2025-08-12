422cd2f1847572c96d2e2fef29c3955ecf882279
arjun <arjun.m010011@gmail.com>
2025-08-12 17:45:46 +0000

    testing again

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 import Header from "./components/Header";
+//testing
 import { Button } from "@/components/ui/button";
 import Footer from "./components/Footer";
 import { Wand2, TrendingUp, GraduationCap } from "lucide-react";
